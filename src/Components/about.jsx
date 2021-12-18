2bf28834ac73d51b4baa742c23fd6072f5388d75
Decode-dinesh <280997dinesh@gmail.com>
2021-12-18 19:54:28 +0530

    final

---
+++ src/Components/about.jsx
@@ -0,0 +1,19 @@
+import "./css/about.css";
+function About()
+{
+    return(
+        <div className="container wrapper">
+            <div className="card about">
+                <div className="card-body about-body">
+                    <div><i class="fas fa-palette"></i></div>
+                    <p><b>Name:</b>Dinesh P</p>
+                    <p><b>Qualification:</b>Msc Physics</p>
+                    <p><b>Contact:</b>280997dinesh@gmail.com</p>
+                </div>
+                <i className="fas fa-feather-alt backicon"></i>
+            </div>
+            
+        </div>
+    );
+}
+export default About;
